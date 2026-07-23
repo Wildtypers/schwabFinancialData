6f13c8ad9d660f9ce1d8d76c145a9ec096c605c3
Wildtypers <wildtypers@gmail.com>
2026-07-22 22:27:37 -0700

    adding initial getenv ability

--- backend/codeBase/main.py
+++ backend/codeBase/main.py
@@ -1,5 +1,12 @@
 import requests
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+app_key = os.getenv("SCHWAB_APP_KEY")
+app_secret = os.getenv("SCHWAB_APP_SECRET")
 
 app = FastAPI()
 
