cb520b2575f81d630a6aeef9bd669e3721ece0ae
Wildtypers <wildtypers@gmail.com>
2026-07-23 00:21:52 -0700

    restructured code for token initian

--- backend/codeBase/main.py
+++ backend/codeBase/main.py
@@ -6,23 +6,51 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-appKey = os.getenv("SCHWAB_APP_KEY")
-appSecret = os.getenv("SCHWAB_APP_SECRET")
-callbackUrl = os.getenv("SCHWAB_CALLBACK_URL")
-authorizationURL= "https://api.schwabapi.com/v1/oauth/authorize?response_type=code&client_id=fnB6k1X6JSFlQHravRt6T9m86AZlkD04&scope=readonly&redirect_uri=https://developer.schwab.com/oauth2-redirect.html"
+clientID = os.getenv("SCHWAB_APP_KEY")
+clientSecret = os.getenv("SCHWAB_APP_SECRET")
+callbackURL = os.getenv("SCHWAB_CALLBACK_URL")
 tokenURL = "https://api.schwabapi.com/v1/oauth/token"
 
-def create_authorization_url():
-    params = {
-        "client_id": appKey,
-        "redirect_url": callbackUrl,
-        "response_type": "code"
-    }
+authorizationURL = (
+    "https://api.schwabapi.com/v1/oauth/authorize"
+    f"?response_type=code"
+    f"&client_id={clientID}"
+    f"&scope=readonly"
+    f"&redirect_uri={callbackURL}"
+)
 
-    return f"{authorizationURL}?{urlencode(params)}"
+def get_tokens(auth_code):
+
+    response = requests.post(
+        tokenURL,
+        data={
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "redirect_uri": callbackURL
+        },
+        auth=(
+            clientID,
+            clientSecret
+        )
+    )
+
+    return response.json()
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/auth/login")
+def login():
+    return {
+        "authorization_url": authorizationURL
+    }
+
+@app.get("/callback")
+def callback(code: str):
+
+    tokens = get_tokens(code)
+
+    return tokens
